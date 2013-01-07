60f74d285e06104125ec48b6b379540807b71d6c
Sam Ritchie <sritchie@twitter.com>
2013-01-06 22:06:26 -0800

    bump to release.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object BijectionBuild extends Build {
   val sharedSettings = Project.defaultSettings ++ Seq(
     organization := "com.twitter",
-    version := "0.1.1",
+    version := "0.1.2-SNAPSHOT",
     scalaVersion := "2.9.2",
     libraryDependencies ++= Seq(
       "org.scalacheck" %% "scalacheck" % "1.10.0" % "test" withSources(),
