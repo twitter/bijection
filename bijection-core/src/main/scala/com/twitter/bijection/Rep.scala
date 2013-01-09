c0e8204e5eb8b077b7e946d088228f86473a18a8
Jed Wesley-Smith <jed@atlassian.com>
2013-01-10 10:57:36 +1100

    example of lifting String into Option[String @@ Rep[Int]]

--- bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
+++ bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
@@ -4,3 +4,11 @@ package com.twitter.bijection
  * Type tag used when a type such as String may contain representations of another type, such as Int or URL.
  */
 trait Rep[A]
+
+object Rep {
+  private val catching = scala.util.control.Exception.catching(classOf[NumberFormatException])
+
+  // for example the following lifts a String into String @@ Rep[Int]
+  // could implicitly add toIntRep syntax as well
+  def stringToIntRep(s: String): Option[String @@ Rep[Int]] = catching.opt(s.toInt) map (_ => Tag(s))
+}
