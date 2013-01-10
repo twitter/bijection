491cfb5158b4e742fbb8908e2b099db1486a4b41
Jed Wesley-Smith <jed@atlassian.com>
2013-01-10 15:09:28 +1100

    Added some docs and reformatted for project style consistency.

--- bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
+++ bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
@@ -4,49 +4,56 @@ import java.net.{ MalformedURLException, URL }
 import java.util.UUID
 
 /**
- * Type tag used when a type such as String may contain representations of another type, such as Int or URL.
+ * Type tag used to indicate that an instance of a type such as String 
+ * contains a valid representation of another type, such as Int or URL.
  */
 trait Rep[A]
 
+/**
+ * Useful HasRep 
+ */
 object Rep {
   // TODO make implicit class in 2.10
   implicit def ToRepOpsPimp[A](a: A) = new ToRepOps(a)
 
+  /**
+   * Adds toRep[B] syntax to elements of type A if there is an implicit HasRep[A, B] in scope.
+   */
   class ToRepOps[A](a: A) {
     def toRep[B](implicit ev: HasRep[A, B]): Option[A @@ Rep[B]] =
       ev.toRep(a)
   }
 
   implicit val StringHasIntRep = new HasRep[String, Int] {
-    def toRep(s: String) = rep(s)(_.toInt)
+    def toRep(s: String) = rep(s) { _.toInt }
   }
 
   implicit val StringHasLongRep = new HasRep[String, Long] {
-    def toRep(s: String) = rep(s)(_.toLong)
+    def toRep(s: String) = rep(s) { _.toLong }
   }
 
   implicit val StringHasByteRep = new HasRep[String, Byte] {
-    def toRep(s: String) = rep(s)(_.toByte)
+    def toRep(s: String) = rep(s) { _.toByte }
   }
 
   implicit val StringHasShortRep = new HasRep[String, Short]  {
-    def toRep(s: String) = rep(s)(_.toShort)
+    def toRep(s: String) = rep(s) { _.toShort }
   }
 
   implicit val StringHasFloatRep = new HasRep[String, Float] {
-    def toRep(s: String) = rep(s)(_.toFloat)
+    def toRep(s: String) = rep(s) { _.toFloat }
   }
 
   implicit val StringHasDoubleRep = new HasRep[String, Double]  {
-    def toRep(s: String) = rep(s)(_.toDouble)
+    def toRep(s: String) = rep(s) { _.toDouble }
   }
 
   implicit val StringHasURLRep = new HasRep[String, URL]  {
-    def toRep(s: String) = rep(s)(new URL(_))
+    def toRep(s: String) = rep(s) { new URL(_) }
   }
 
   implicit val StringHasUUIDRep = new HasRep[String, UUID]  {
-    def toRep(s: String) = rep(s)(UUID.fromString)
+    def toRep(s: String) = rep(s) { UUID.fromString }
   }
   
   private val catching = 
