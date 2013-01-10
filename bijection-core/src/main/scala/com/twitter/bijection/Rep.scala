1a9ee52cd81ab5335179a13cd92d109471fb8259
Jed Wesley-Smith <jed@atlassian.com>
2013-01-10 13:13:44 +1100

    Adds "toRep[A]" syntax with import Rep._ to provide the ability to take a naked type and lift it into its Rep if it matches. eg.
    scala> "123".toRep[Int]
    res2: Option[com.twitter.bijection.package.@@[java.lang.String,com.twitter.bijection.Rep[Int]]] = Some(123)
    
    scala> "123a".toRep[Int]
    res3: Option[com.twitter.bijection.package.@@[java.lang.String,com.twitter.bijection.Rep[Int]]] = None

--- bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
+++ bijection-core/src/main/scala/com/twitter/bijection/Rep.scala
@@ -1,14 +1,69 @@
 package com.twitter.bijection
 
+import java.net.{ MalformedURLException, URL }
+import java.util.UUID
+
 /**
  * Type tag used when a type such as String may contain representations of another type, such as Int or URL.
  */
 trait Rep[A]
 
 object Rep {
-  private val catching = scala.util.control.Exception.catching(classOf[NumberFormatException])
+  // TODO make implicit class in 2.10
+  implicit def ToRepOpsPimp[A](a: A) = new ToRepOps(a)
 
-  // for example the following lifts a String into String @@ Rep[Int]
-  // could implicitly add toIntRep syntax as well
-  def stringToIntRep(s: String): Option[String @@ Rep[Int]] = catching.opt(s.toInt) map (_ => Tag(s))
+  class ToRepOps[A](a: A) {
+    def toRep[B](implicit ev: HasRep[A, B]): Option[A @@ Rep[B]] =
+      ev.toRep(a)
+  }
+
+  implicit val StringHasIntRep = new HasRep[String, Int] {
+    def toRep(s: String) = rep(s)(_.toInt)
+  }
+
+  implicit val StringHasLongRep = new HasRep[String, Long] {
+    def toRep(s: String) = rep(s)(_.toLong)
+  }
+
+  implicit val StringHasByteRep = new HasRep[String, Byte] {
+    def toRep(s: String) = rep(s)(_.toByte)
+  }
+
+  implicit val StringHasShortRep = new HasRep[String, Short]  {
+    def toRep(s: String) = rep(s)(_.toShort)
+  }
+
+  implicit val StringHasFloatRep = new HasRep[String, Float] {
+    def toRep(s: String) = rep(s)(_.toFloat)
+  }
+
+  implicit val StringHasDoubleRep = new HasRep[String, Double]  {
+    def toRep(s: String) = rep(s)(_.toDouble)
+  }
+
+  implicit val StringHasURLRep = new HasRep[String, URL]  {
+    def toRep(s: String) = rep(s)(new URL(_))
+  }
+
+  implicit val StringHasUUIDRep = new HasRep[String, UUID]  {
+    def toRep(s: String) = rep(s)(UUID.fromString)
+  }
+  
+  private val catching = 
+    scala.util.control.Exception.catching(
+      classOf[NumberFormatException]
+    , classOf[MalformedURLException]
+    , classOf[IllegalArgumentException]
+    )
+
+  // catch and return option
+  private def rep[A, B](a: A)(partial: A => B): Option[A @@ Rep[B]] =
+    catching.opt(partial(a)) map (_ => Tag(a))
+}
+
+/**
+ * Type class for summoning the function that can check whether the instance can be tagged with Rep
+ */
+trait HasRep[A, B] {
+  def toRep(a: A): Option[A @@ Rep[B]]
 }
