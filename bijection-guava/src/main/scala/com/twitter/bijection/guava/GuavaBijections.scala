d288a5b9808cff8fc2f9c17ff4cf04546444c18c
Sam Ritchie <sritchie@twitter.com>
2013-01-23 13:21:18 -0800

    convert between scala and guava functions.

--- bijection-guava/src/main/scala/com/twitter/bijection/guava/GuavaBijections.scala
+++ bijection-guava/src/main/scala/com/twitter/bijection/guava/GuavaBijections.scala
@@ -18,6 +18,7 @@ package com.twitter.bijection.guava
 
 import com.google.common.base.Optional
 import com.twitter.bijection.Bijection
+import com.google.common.base.{ Function => GFn }
 
 /**
  * Bijections between Scala and Guava.
@@ -28,4 +29,17 @@ object GuavaBijections {
     Bijection.build[Optional[T], Option[U]]
       { opt => if (opt.isPresent) Some(bij(opt.get)) else None }
       { opt => if (opt.isDefined) Optional.of[T](bij.invert(opt.get)) else Optional.absent[T] }
+
+  /**
+   * Converts a scala Function1 into a Guava Function.
+   */
+  implicit def guavaFn2ScalaFn[A, B, C, D](implicit bij1: Bijection[A, B], bij2: Bijection[C, D])
+  : Bijection[A => C, GFn[B, D]] =
+    new Bijection[A => C, GFn[B, D]] {
+      def apply(fn: A => C) =
+        new GFn[B, D] {
+          override def apply(b: B): D = bij2.apply(fn(bij1.invert(b)))
+        }
+      override def invert(fn: GFn[B,D]) = { a => bij2.invert(fn.apply(bij1.apply(a))) }
+    }
 }
