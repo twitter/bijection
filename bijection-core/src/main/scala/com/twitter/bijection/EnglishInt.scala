401b8da3be85a309aed48aaa87b6f3308a20f29a
krishnanraman <kraman@twitter.com>
2013-01-28 17:00:35 -0800

    Added unit tests. Made helper methods private as per Oscar.

--- bijection-core/src/main/scala/com/twitter/bijection/EnglishInt.scala
+++ bijection-core/src/main/scala/com/twitter/bijection/EnglishInt.scala
@@ -8,21 +8,17 @@ six hundred ninety one million one hundred seventy thousand five hundred fifteen
 23 is twenty three
 234 is two hundred thirty four
 
-Intention: Can operate on strings as numbers, like so
-add("three", "twenty four") = "twenty seven"
-mult, div etc.
-
 Author: Krishnan Raman, kraman@twitter.com
 */
 
 package com.twitter.bijection
 
-trait EnglishNumeralsBijection {
+case class EnglishInt(get: String)
 
-  implicit val Int2String: Bijection[Int, String @@ Rep[Int]] =
-    new Bijection[Int, String @@ Rep[Int]] {
-      def apply(num: Int) = Tag(toEnglish(num).get)
-      override def invert(s: String @@ Rep[Int]) = fromEnglish(s).get
+object EnglishInt {
+  implicit val bijectionToInt: Bijection[Int, EnglishInt] = new Bijection[Int, EnglishInt] {
+      def apply(num: Int):EnglishInt = EnglishInt(toEnglish(num).get)
+      override def invert(s: EnglishInt):Int = fromEnglish(s.get).get
   }
 
   val (t,d,k,m,g) = (10, 100,1000,1000*1000, 1000*1000*1000)
@@ -34,20 +30,26 @@ trait EnglishNumeralsBijection {
   val word2num:Map[String,Int] = (units++tens++teens++tenmult).map( kv=> (kv._2, kv._1))
   val s = " "
 
-  def toEnglish(num:Int):Option[String] = {
+  // a helper function that converts num of type Int to a String
+  // num belongs to exactly one of several bins
+  // [0,20], [20,d], [d,k], [k,m],[m,g]
+  // given the bin, we divide by suitable divisor to obtain quotient & remainder
+  // the quotient & remainder are converted to Enmglish recurisively
+  private def toEnglish(num:Int):Option[String] = {
     num match {
       case num if num < 0 => None
       case num if num > g => None
       case num if num < 20 => Some(all(num))
-      case num if num < d => f(num,t)
-      case num if num < k => f(num,d)
-      case num if num < m => f(num,k)
-      case num if num < g => f(num,m)
+      case num if num < d => divide(num,t)
+      case num if num < k => divide(num,d)
+      case num if num < m => divide(num,k)
+      case num if num < g => divide(num,m)
       case _ => None
     }
   }
 
-  def f(num:Int, div:Int):Option[String] = {
+  // a helper function that recursively converts num of type Int to a string
+  private def divide(num:Int, div:Int):Option[String] = {
     val (quo, rem) = (num/div, num%div)
     if( div == t) {
         Some(tens(quo*10) + (if (rem>0) (s + units(rem)) else ""))
@@ -58,7 +60,8 @@ trait EnglishNumeralsBijection {
     }
   }
 
-  def fromEnglish(str:String):Option[Int] = {
+  // a helper function that converts valid strings to Int, invalid to None
+  private def fromEnglish(str:String):Option[Int] = {
     val list = str.split(s).toList // strip spaces
     val valid = list.map(word2num.keySet.contains).foldLeft(true)(_&&_)
     if (valid) {
@@ -67,7 +70,8 @@ trait EnglishNumeralsBijection {
     } else None
   }
 
-  def numlist2int(numbers:List[Int]):Int = {
+  // a helper function that recursively converts List[Int] to Int
+  private def numlist2int(numbers:List[Int]):Int = {
     val (id,ik,im) = (numbers.indexOf(d),numbers.indexOf(k),numbers.indexOf(m))
     val has_100 = id > -1
     val has_higher = ( ik > -1 || im > -1 )
@@ -89,7 +93,8 @@ trait EnglishNumeralsBijection {
     }
   }
 
-  def fold(numbers:List[Int]):Int = {
+  // folds List[Int] to Int
+  private def fold(numbers:List[Int]):Int = {
     val res = numbers.foldLeft(0,0) ((adderaccum:(Int,Int), b:Int) => {
       val (adder,accum) = (adderaccum._1, adderaccum._2)
       if( b == 100 || b == 1000 || b == 1000*1000 || b == 1000*1000*1000) {
@@ -101,10 +106,10 @@ trait EnglishNumeralsBijection {
     res._1 + res._2
   }
 
-// fold Lists where 100 occurs before 1000, or 1000,000
-// eg. (7, 100, 1000) = 700,000
-// eg. (2,100, 1000000) = 200,000,000
-  def fold100(numbers:List[Int]):Int = {
+  // fold Lists where 100 occurs before 1000, or 1000,000
+  // eg. (7, 100, 1000) = 700,000
+  // eg. (2,100, 1000000) = 200,000,000
+  private def fold100(numbers:List[Int]):Int = {
     val res = numbers.foldLeft(0,0) ((adderaccum:(Int,Int), b:Int) => {
       val (adder,accum) = (adderaccum._1, adderaccum._2)
       if( b == 100 ) {
@@ -116,49 +121,5 @@ trait EnglishNumeralsBijection {
       })
     res._1 + res._2
   }
-
-  def add(a:String, b:String):Option[String] = {
-    val (aa,bb) = (fromEnglish(a), fromEnglish(b))
-    if( aa.isDefined && bb.isDefined) toEnglish(aa.get+bb.get) else None
-  }
-
-  def mult(a:String, b:String):Option[String] = {
-    val (aa,bb) = (fromEnglish(a), fromEnglish(b))
-    if( aa.isDefined && bb.isDefined) toEnglish(aa.get*bb.get) else None
-  }
-
-  def div(a:String, b:String):Option[String] = {
-    val (aa,bb) = (fromEnglish(a), fromEnglish(b))
-    if( aa.isDefined && bb.isDefined) toEnglish(aa.get/bb.get) else None
-  }
 }
-
-/* belongs in tests
-object EnglishNumeralsApp extends App{
-  override def main(argv:Array[String]):Unit = {
-    new {
-      val x = "foo"
-      } with EnglishNumerals {
-
-        (0 to 100).foreach( i => printf("%d is %s\n", i, toEnglish(i).get))
-        (100 to 10000 by 123).foreach( i => printf("%d is %s\n", i, toEnglish(i).get))
-        (10000 to 1000000 by 12345).foreach( i => printf("%d is %s\n", i, toEnglish(i).get))
-
-      printf("2 is %s\n", toEnglish(2))
-      printf("23 is %s\n", toEnglish(23))
-      printf("234 is %s\n", toEnglish(234))
-      printf("2345 is %s\n", toEnglish(2345))
-      printf("23456 is %s\n", toEnglish(23456))
-      printf("234567 is %s\n", toEnglish(234567))
-
-      (0 to 11000).foreach( i => printf("%s is %d\n", toEnglish(i).get, fromEnglish(toEnglish(i).get).get))
-      (11000 to 1000*1000*1000 by 12345).foreach( i => printf("%s is %d\n", toEnglish(i).get, fromEnglish(toEnglish(i).get).get))
-
-      printf("%s plus %s is %s\n", "twenty three", "forty nine", add( "twenty three", "forty nine" ).get)
-      printf("%s times %s is %s\n", "fifteen", "six", mult("fifteen", "six" ).get)
-      printf("%s divided by %s is %s\n", "fourteen", "two", div("fourteen", "two").get)
-    }
-  }
-}
-*/
 
