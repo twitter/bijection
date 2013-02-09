e0978d577e6e86773d2b00462483025fa147a8c6
Oscar Boykin <oscar@twitter.com>
2013-02-09 12:34:26 -0800

    Removes commented debug from Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,6 @@ object BijectionBuild extends Build {
 
     parallelExecution in Test := true,
 
-    //scalacOptions ++= Seq("-unchecked", "-deprecation", "-Xlog-implicits"),
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
 
     // Publishing options:
