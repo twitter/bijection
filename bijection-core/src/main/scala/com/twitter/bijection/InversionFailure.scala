8263ebbc71e33352303ae3ac43017dd726c360fb
softprops <d.tangren@gmail.com>
2013-06-23 15:11:19 -0400

    introduce InversionFailure as its own type

---
+++ bijection-core/src/main/scala/com/twitter/bijection/InversionFailure.scala
@@ -0,0 +1,51 @@
+/*
+Copyright 2012 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.twitter.bijection
+
+import scala.util.control.Exception.allCatch
+
+/**
+ *  Factory for producing InversionFailures
+ */
+object InversionFailure {
+  /**
+   *  The analog of Exception.allCatch either where exceptions
+   *  are wrapped by the InversionFailure type
+   */
+  def attempt[A, B](b: B)(fn: B => A): Attempt[A] =
+    allCatch.either(fn(b)).left.map(InversionFailure(b, _))
+
+  /**
+   *  Produces a new InversionFailure of a given type
+   */
+  def apply[B](b: B): InversionFailure =
+    new InversionFailure(b, new UnsupportedOperationException)
+
+  /**
+   *  Produces a failed Attempt
+   */ 
+  def failedAttempt[A, B](b: B): Attempt[A] =
+    Left(apply(b))
+}
+
+/**
+ *  When Injection inversion attempts are known to fail,
+ *  the attempt will encode an InversionFailure to represent
+ *  that failure
+ */
+case class InversionFailure(val failed: Any, ex: Throwable)
+  extends UnsupportedOperationException("Failed to invert: %s" format(failed), ex)
+
