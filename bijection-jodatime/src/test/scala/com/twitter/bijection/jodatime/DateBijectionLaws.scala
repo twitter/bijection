e71bf4b35c574b57b2934d7979220816b8350dd8
Haidar Hadi <haidar_hadi_205@yahoo.com>
2013-07-14 14:03:31 -0700

    added injections tests

--- bijection-jodatime/src/test/scala/com/twitter/bijection/jodatime/DateBijectionLaws.scala
+++ bijection-jodatime/src/test/scala/com/twitter/bijection/jodatime/DateBijectionLaws.scala
@@ -5,28 +5,24 @@ import org.scalacheck.Gen._
 import org.scalacheck.Arbitrary
 import org.scalacheck.Prop._
 
-import com.twitter.bijection.{ Bijection,BaseProperties , ImplicitBijection}
+import com.twitter.bijection.{ Bijection, BaseProperties, ImplicitBijection }
 //import Conversion.asMethod
 import java.util.Date
 import org.joda.time.DateTime
 import com.twitter.bijection._
 
+object DateBijectionsLaws extends Properties("DateBijections") with BaseProperties with DateBijections with DateInjections {
 
-
-
-
-object DateBijectionsLaws extends Properties("DateBijections") with BaseProperties with DateBijections {
-  
-
- 
-  
-  implicit val Long : Arbitrary [Long] = arbitraryViaFn { (s: DateTime) => (s.getMillis()) }
+  //bijections test
+  implicit val Long: Arbitrary[Long] = arbitraryViaFn { (s: DateTime) => (s.getMillis()) }
   property("Joda <=> Long") = isBijection[DateTime, Long]
-  
-  
-  implicit val DateTime : Arbitrary [DateTime] = arbitraryViaFn { (s: Date) => ( new DateTime (s) ) }
+
+  implicit val DateTime: Arbitrary[DateTime] = arbitraryViaFn { (s: Date) => (new DateTime(s)) }
   property("Date <=> Joda") = isBijection[Date, DateTime]
-  
-  
+
+  //injection test
+  property("round trips Date -> String") = isLooseInjection[Date, String]
+  property("round trips Joda -> String") = isLooseInjection[DateTime, String]
+  property("round trips Joda -> Date") = isLooseInjection[DateTime, Date]
 
 }
