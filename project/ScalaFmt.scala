0821e0c76393a43f06f66fcf7da11ef1e653589a
Chris Hodapp <clhodapp1@gmail.com>
2016-11-26 01:41:09 -0800

    Fully disable scalafmt on 2.10

--- project/ScalaFmt.scala
+++ project/ScalaFmt.scala
@@ -12,27 +12,35 @@ object ScalaFmt extends AutoPlugin with ScalaFmtKeys {
 
   lazy val scalafmtStub = {
     Project(base = file("project/scalafmt"), id = "scalafmt-stub").settings(
-      libraryDependencies += "com.geirsson" %% "scalafmt-cli" % "0.4.10",
-      mainClass in Compile := Some("org.scalafmt.cli.Cli")
+      libraryDependencies ++= {
+        if (!scalaVersion.value.startsWith("2.10")) Seq("com.geirsson" %% "scalafmt-cli" % "0.4.10")
+        else Seq.empty
+      },
+      mainClass in Compile := {
+        if (!scalaVersion.value.startsWith("2.10")) Some("org.scalafmt.cli.Cli")
+        else None
+      }
     )
   }
   override def extraProjects = Seq(scalafmtStub)
   override def projectSettings = Seq(
     scalafmtTest := Def.taskDyn {
-      val scalaSourceFiles = {
-        (unmanagedSources in Compile).value ++ (unmanagedSources in Test).value
-      }.filter(_.ext == "scala").flatMap { f =>
-        val maybeFile = f relativeTo (baseDirectory in ThisBuild).value
-        maybeFile.toSeq
-      }.mkString(",")
-      val args = Seq(
-        "",
-        "--test",
-        "--config=.scalafmt.conf",
-        "--files="++scalaSourceFiles
-      ).mkString(" ")
-      if (scalaSourceFiles.nonEmpty) {
-        (run in (scalafmtStub, Compile)).toTask(args)
+      if (!scalaVersion.value.startsWith("2.10")) {
+        val scalaSourceFiles = {
+          (unmanagedSources in Compile).value ++ (unmanagedSources in Test).value
+        }.filter(_.ext == "scala").flatMap { f =>
+          val maybeFile = f relativeTo (baseDirectory in ThisBuild).value
+          maybeFile.toSeq
+        }.mkString(",")
+        val args = Seq(
+          "",
+          "--test",
+          "--config=.scalafmt.conf",
+          "--files="++scalaSourceFiles
+        ).mkString(" ")
+        if (scalaSourceFiles.nonEmpty) {
+          (run in (scalafmtStub, Compile)).toTask(args)
+        } else Def.task { () }
       } else Def.task { () }
     }.value
   )
